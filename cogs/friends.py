eb2f7ff2b63739af9f98c257420bfe33393524cf
andreashennestad <59829447+andreashennestad@users.noreply.github.com>
2021-05-05 11:46:03 +0200

    updates to cogs and created media folder

--- cogs/friends.py
+++ cogs/friends.py
@@ -26,6 +26,17 @@ class Friends(commands.Cog):
             if str(message.author) == "Rylance#6969":
                 await message.channel.send('Lewis! Have you seen Fredrik?? Please find him, he owes me money!')
 
+            if str(message.author) == "akvanvig#4328":
+                await message.channel.send('Hei Anders! Visste du dette?: Mao Zedong (tradisjonell kinesisk: 毛澤東, '
+                                           'forenklet kinesisk: 毛泽东, pinyin: Máo Zédōng; Wade-Giles: Mao Tse-tung, '
+                                           'født 26. desember 1893 i Shaoshan i Hunan i Kina, død 9. september 1976 i '
+                                           'Beijing) var en kinesisk kommunist, statsleder, revolusjonær og '
+                                           'teoretiker. Han ledet Kinas kommunistparti til seier i den kinesiske '
+                                           'borgerkrigen og grunnla Folkerepublikken Kina i 1949, som han ledet fram '
+                                           'til sin død i 1976. Mao er også grunnleggeren av en retning innen '
+                                           'marxismen-leninismen kjent som maoismen.')
+                await message.channel.send("https://media.snl.no/media/67119/standard_compressed_sz8318b2.jpg")
+
             else:
                 await message.channel.send('Hei! Har du sett Fredrik? Kan du be han si hei til meg?')
 
