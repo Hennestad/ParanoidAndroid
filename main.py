2bd17204ea3c6c4e6cd81bf4b25a30f42db4c77a
andreashennestad <59829447+andreashennestad@users.noreply.github.com>
2021-05-05 10:48:15 +0200

    Security update regarding token

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import os
 from discord.ext import commands
 from discord.ext.commands import bot
 
-
 # Symbol used for calling commands
 client = commands.Bot(command_prefix='#')
 
@@ -34,6 +33,8 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
+# runs bot
+with open("token", "r", encoding="utf-8") as f:
+    bottoken = f.read()
 
-# Runs client
-client.run('NzgxNjc5NDk5MzM4MzE3OTA2.X8BJww.ZO7fGmKah3cE1uCw17ZOpksHOMI')
+client.run(bottoken)
